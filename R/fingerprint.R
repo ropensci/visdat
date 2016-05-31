9c5d04be03d2eb68662b8e6032f25c2432ce45e4
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-05-31 23:30:23 +1000

    misc and remove fingerprint from export
    fingerprint doesn’t need to be exported for the user

--- R/fingerprint.R
+++ R/fingerprint.R
@@ -1,10 +1,9 @@
 #' A utility function for \code{vis_dat}
 #'
-#' \code{fingerprint} takes the "fingerprint" of a dataframe, and (currently) replaces the contents (x) with the class of a given object, unless it is missing (coded as NA), in which case it leaves it as NA. The name "fingerprint" is taken from the csv-fingerprint, of which this package is based.
+#' \code{fingerprint} is an internal function that takes the "fingerprint" of a dataframe, and (currently) replaces the contents (x) with the class of a given object, unless it is missing (coded as NA), in which case it leaves it as NA. The name "fingerprint" is taken from the csv-fingerprint, of which this package is based.
 #'
 #' @param x a vector
 #'
-#' @export
 fingerprint <- function(x){
 
   # is the data missing?
--- tests/testthat/test_variable_spaces.R
+++ tests/testthat/test_variable_spaces.R
@@ -1,4 +1,4 @@
-# Currently commented out
+#
 # messy_names <- data_frame(`Sepal Width` = iris$Sepal.Width,
 #                           `Sepal Length` = iris$Sepal.Length,
 #                           `Petal Length` = iris$Petal.Length,
@@ -7,7 +7,5 @@
 # context("Test names with spaces")
 #
 # test_that("vis_dat works on dataframes with irregular variable names", {
-#   expect_success(
-#     vis_dat(messy_names)
-#     )
+#   expect_ (vis_dat(messy_names))
 # })
