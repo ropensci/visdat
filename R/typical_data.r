3f5d67c2011bb86c6b6d3eece939d682ea5908fb
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-08-18 08:36:31 +1000

    create typical_data and typical_larder_data
    These are new example datasets for visdat, created with visdat

---
+++ R/typical_data.r
@@ -0,0 +1,19 @@
+#' A small toy dataset of imaginary people
+#'
+#' A dataset containing information about some randomly generated people, created using the excellent \code{wakefield} package. It is created as deliberately messy dataset.
+#'
+#' @format A data frame with 5000 rows and 11 variables:
+#' \describe{
+#'   \item{ID}{Unique identifier for each individual}
+#'   \item{Race}{Race for each individual, "Black", "White", "Hispanic", "Asian", "Other", "Bi-Racial", "Native", and "Hawaiin"}
+#'   \item{Age}{Age of each individul}
+#'   \item{Sex}{Male or female}
+#'   \item{Hour}{}
+#'   \item{IQ}{}
+#'   \item{Height}{}
+#'   \item{Died}{}
+#'   \item{Smoker}{}
+#'   \item{Income}{}
+#'   \item{age}{}
+#' }
+"typical_data"
---
+++ R/typical_larger_data.R
@@ -0,0 +1,57 @@
+#' A small toy dataset of imaginary people
+#'
+#' A wider dataset than \code{typical_data} containing information about some randomly generated people, created using the excellent \code{wakefield} package. It is created as deliberately messy dataset.
+#'
+#' @format A data frame with 300 rows and 49 variables:
+#' \describe{
+#'   \item{Age}{}
+#'   \item{Animal}{}
+#'   \item{Answer}{}
+#'   \item{Area}{}
+#'   \item{Car}{}
+#'   \item{Children}{}
+#'   \item{Coin}{}
+#'   \item{Color}{}
+#'   \item{Date}{}
+#'   \item{Death}{}
+#'   \item{Dice}{}
+#'   \item{DNA}{}
+#'   \item{DOB}{}
+#'   \item{Dummy}{}
+#'   \item{Education}{}
+#'   \item{Employment}{}
+#'   \item{Eye}{}
+#'   \item{Grade}{}
+#'   \item{Grade_Level}{}
+#'   \item{Group}{}
+#'   \item{hair}{}
+#'   \item{Height}{}
+#'   \item{Income}{}
+#'   \item{Browser}{}
+#'   \item{IQ}{}
+#'   \item{Language}{}
+#'   \item{Level}{}
+#'   \item{Likert}{}
+#'   \item{Lorem_Ipsum}{}
+#'   \item{Marital}{}
+#'   \item{Military}{}
+#'   \item{Month}{}
+#'   \item{Name}{}
+#'   \item{Normal}{}
+#'   \item{Political}{}
+#'   \item{Race}{}
+#'   \item{Religion}{}
+#'   \item{SAT}{}
+#'   \item{Sentence}{}
+#'   \item{Sex_1}{}
+#'   \item{Sex_2}{}
+#'   \item{Smokes}{}
+#'   \item{Speed}{}
+#'   \item{State}{}
+#'   \item{String}{}
+#'   \item{Upper}{}
+#'   \item{Valid}{}
+#'   \item{Year}{}
+#'   \item{Zi}{}
+#' }
+"typical_larger_data"
