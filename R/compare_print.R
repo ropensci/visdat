4efbaab4af7cc01015caf069752cc29eee5dc8e8
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-08-18 08:37:11 +1000

    added vis_compare
    Only works for data frames of the same dimension!

---
+++ R/compare_print.R
@@ -0,0 +1,18 @@
+#' A utility function for \code{vis_dat}
+#'
+#' \code{compare_print} is an internal function that takes creates a dataframe with information about where there are differences in the dataframe. This function is used in "vis_compare". It does a simple evaluation on the data (df1 == df2) and (currently) replaces the "true" (the same) with "Same" and FALSE with "Different", unless it is missing (coded as NA), in which case it leaves it as NA.
+#'
+#' @param x a vector
+#'
+compare_print <- function(x){
+
+  dplyr::if_else(x == "TRUE",
+                 true = "same",
+                 false = "different",
+                 missing = "missing")
+
+
+} # end function
+
+# how can I capture when something changes from a value to NA, or, from NA to another value?
+
--- R/vis_compare.R
+++ R/vis_compare.R
@@ -1,23 +1,38 @@
 #' compare two dataframes and see where they are different.
 #'
-#' \code{vis_compare}, like the other vis_* families, gives an at-a-glance ggplot of a particular feature. In this case, it is the difference between two dataframes. This function has not been implemented yet, but this serves as a note of how it might work. It would be very similar to vis_miss, where you basically colouring cells according to "match" and "non-match". The code for this would be pretty crazy simple. `x <- 1:10; y <- c(1:5, 10:14) ;x == y` returns ` [1]  TRUE  TRUE  TRUE  TRUE  TRUE FALSE FALSE FALSE FALSE FALSE` Here black could indicate a match, and white a non match. One of the challenges with this would be cases where the datasets are different dimensions. One option could be to return as a message the columns that are not in the same dataset. Matching rows could be done by row number, and just lopping off the trailing ones and spitting out a note. Then, if the user wants, it could use an ID/key to match by.
+#' \code{vis_compare}, like the other vis_* families, gives an at-a-glance ggplot of a dataset, but in this case hones in on visualising **two** different dataframes, which currently need to be exactly the same dimension. This function has not been implemented yet, but this serves as a note of how it might work. It would be very similar to vis_miss, where you basically colouring cells according to "match" and "non-match". The code for this would be pretty crazy simple. `x <- 1:10; y <- c(1:5, 10:14) ;x == y` returns ` [1]  TRUE  TRUE  TRUE  TRUE  TRUE FALSE FALSE FALSE FALSE FALSE` Here black could indicate a match, and white a non match. One of the challenges with this would be cases where the datasets are different dimensions. One option could be to return as a message the columns that are not in the same dataset. Matching rows could be done by row number, and just lopping off the trailing ones and spitting out a note. Then, if the user wants, it could use an ID/key to match by.
 #'
 #' @param df1 the first dataframe to compare to
 #'
 #' @param df2 the second dataframe to compare to
 #'
 #'
+#' @examples
+#'
+#' # make a new dataset of iris that contains some NA values
+#' iris_diff <- iris
+#' iris_diff[sample(1:150, 30),sample(1:4, 2)] <- NA
+#'
+#' library(visdat)
+#' vis_compare(iris_diff, iris)
+#'
+#'
 #' @export
-# NOTE: This function is not yet complete, and is unexported as it is still mostly in the idea phase
 
 vis_compare <- function(df1,
                         df2){
+
+  warning("vis_compare is still in BETA! If you have suggestions or errors, post an issue at https://github.com/njtierney/visdat/issues")
 
   # could add a parameter, "sort_match", to help with
   # sort_match logical TRUE/FALSE. TRUE arranges the columns in order of most matches.
 
   # make a TRUE/FALSE matrix of the data.
   # This tells us whether it is the same (true) as the other dataset, or not (false)
+
+  if(dim(df1) != dim(df2))
+    stop("Dimensions of df1 and df2 are not the same! Unfortunately at this stage vis_compare only handles dataframes of the exact same dimension. Sorry!")
+
 
   v_identical <- Vectorize(identical)
 
