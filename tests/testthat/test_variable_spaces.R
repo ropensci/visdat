d441d7493ccd6a020b11304728d286963e3f6e3c
Nicholas Tierney <nicholas.tierney@gmail.com>
2016-12-28 20:22:56 +1000

    minor update to broken tests...

--- tests/testthat/test_variable_spaces.R
+++ tests/testthat/test_variable_spaces.R
@@ -1,11 +1,13 @@
-#
-# messy_names <- data_frame(`Sepal Width` = iris$Sepal.Width,
-#                           `Sepal Length` = iris$Sepal.Length,
-#                           `Petal Length` = iris$Petal.Length,
-#                           `Species Names` = iris$Species)
-#
 # context("Test names with spaces")
 #
+# library(visdat)
+#
+# messy_names <- dplyr::data_frame(`Sepal Width` = iris$Sepal.Width,
+#                                  `Sepal Length` = iris$Sepal.Length,
+#                                  `Petal Length` = iris$Petal.Length,
+#                                  `Species Names` = iris$Species)
+#
+#
 # test_that("vis_dat works on dataframes with irregular variable names", {
-#   expect_ (vis_dat(messy_names))
+#   expect_success(vis_dat(messy_names))
 # })
