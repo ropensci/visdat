f8b21d5634de7dca6e411390280511c48f9c5663
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-07-02 02:10:35 +0200

    improved testing with vdiffr

--- tests/testthat/test-vis-dat.R
+++ tests/testthat/test-vis-dat.R
@@ -4,11 +4,20 @@ context("vis_dat")
 vis_dat_plot <- vis_dat(airquality)
 vis_dat_plot_sort_type <- vis_dat(airquality, sort_type = FALSE)
 vis_dat_plot_pal_qual <- vis_dat(airquality, palette = "qual")
-vis_dat_plot_pal_qual <- vis_dat(airquality, palette = "cb_safe")
+vis_dat_plot_pal_cb <- vis_dat(airquality, palette = "cb_safe")
 
 test_that("vis_dat creates the right plot",{
-  vdiffr::expect_doppelganger("vis_dat vanilla", vis_dat_plot)
-  vdiffr::expect_doppelganger("vis_dat sort_type", vis_dat_plot)
-  vdiffr::expect_doppelganger("vis_dat qualitative palette", vis_dat_plot)
-  vdiffr::expect_doppelganger("vis_dat colourblind safe palette", vis_dat_plot)
+  vdiffr::expect_doppelganger("vis_dat vanilla",
+                              vis_dat_plot)
+  vdiffr::expect_doppelganger("vis_dat sort_type",
+                              vis_dat_plot_sort_type)
+  vdiffr::expect_doppelganger("vis_dat qualitative palette",
+                              vis_dat_plot_pal_qual)
+  vdiffr::expect_doppelganger("vis_dat colourblind safe palette",
+                              vis_dat_plot_pal_cb)
+})
+
+
+test_that("vis_dat fails when the wrong palette is provided",{
+  testthat::expect_error(vis_dat(airquality, palette = "wat"))
 })
--- tests/testthat/test-vis-guess.R
+++ tests/testthat/test-vis-guess.R
@@ -2,7 +2,17 @@ context("vis_guess")
 
 # try out all the options
 vis_guess_plot <- vis_guess(airquality)
+vis_guess_plot_cb_safe <- vis_guess(airquality, palette = "cb_safe")
+vis_guess_plot_qual <- vis_guess(airquality, palette = "qual")
+vis_guess_plot_default <- vis_guess(airquality, palette = "default")
 
-test_that("vis_miss_ly creates the right plot",{
+test_that("vis_guess creates the right plot",{
   vdiffr::expect_doppelganger("vis_guess vanilla", vis_guess_plot)
+  vdiffr::expect_doppelganger("vis_guess cb safe", vis_guess_plot_cb_safe)
+  vdiffr::expect_doppelganger("vis_guess qual", vis_guess_plot_qual)
+  vdiffr::expect_doppelganger("vis_guess default", vis_guess_plot_default)
+})
+
+test_that("vis_guess fails when the wrong palette is provided",{
+  testthat::expect_error(vis_guess(airquality, palette = "wat"))
 })
