22a09b592aeccea2bdcbed0e5badc515135f6a85
Nicholas Tierney <nicholas.tierney@gmail.com>
2017-07-03 17:59:19 +0200

    change data to typical_data_large to help with tab completion

---
+++ data-raw/create-typical-data-large.R
@@ -0,0 +1,6 @@
+# the following code was used to create this data
+library(wakefield)
+set.seed(1214)
+typical_data_large <- r_data_theme(n = 300, data_theme = "the_works")
+
+use_data(typical_data_large)
