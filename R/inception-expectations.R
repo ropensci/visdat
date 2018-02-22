35ea312ffbd7b7bcb53cd468fd2df3b9b4863815
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-02-23 00:01:00 +1100

    magic rlang

--- R/inception-expectations.R
+++ R/inception-expectations.R
@@ -15,20 +15,24 @@ expect_frame(airquality, ~.x == 5.1) %>%
 
 expect_frame2 <- function(data, ...){
   funs <- purrr::map(quos(...), lang)
-  lst_cols <- purrr::map2(data, seq_along(data), ~ .x[[.y]])
-  purrr::map(funs, ~ purrr::map2(data, seq_along(data), ~ .x[[.y]]))
-  funs_mod <- purrr::map(funs, lang_modify, data)
+  funs_mod <- purrr::map(data, ~ purrr::map(funs, lang_modify, .))
 
-  purrr::map(funs_mod, eval_tidy)
-  # return(eval_tidy(funs_mod))
-
-  # purrr::map(funs, function(x) x(airquality[[1]]))
-  # purrr::map(data,  function(x) purrr::map(funs, ~ .(x)))
+  purrr::map_dfc(funs_mod, ~ unlist(purrr::map(., eval_tidy), recursive = FALSE, use.names = FALSE))
 
 }
 
-expect_frame2(airquality, is.integer)
-jvis_expect(iris,
+expect_frame2(airquality, function(x) x == -1, function(x) x == 2)
+
+expect_frame3 <- function(data, ...){
+  funs <- purrr::map(quos(...), lang)
+  funs_mod <- purrr::map(data, ~ purrr::map(funs, lang_modify, .))
+
+  purrr::map_dfc(funs_mod, ~ unlist(purrr::map(., eval_tidy), recursive = FALSE, use.names = FALSE))
+
+}
+
+expect_frame3(airquality, ~ .x == -1, ~ .x == 2)
+vis_expect(iris,
            "ones" =  ~.x == -1,
            "twos" = ~.x == 2)
 
