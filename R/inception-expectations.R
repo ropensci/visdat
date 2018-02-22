3c4c842685f81e8387bc6d2c09a37f86069bfcd9
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-02-23 09:20:46 +1100

    added a third iteration of expect_frame

--- R/inception-expectations.R
+++ R/inception-expectations.R
@@ -2,9 +2,7 @@
 library(rlang)
 
 expect_frame <- function(data, expect){
-  my_fun <- rlang::as_function(expect)
-
-  print(my_fun)
+  my_fun <- purrr::as_mapper(expect)
 
   purrr::map_dfc(data, my_fun)
 
@@ -23,17 +21,18 @@ expect_frame2 <- function(data, ...){
 
 expect_frame2(airquality, function(x) x == -1, function(x) x == 2)
 
-expect_frame3 <- function(data, ...){
-  funs <- purrr::map(quos(...), lang)
-  funs_mod <- purrr::map(data, ~ purrr::map(funs, lang_modify, .))
-
-  purrr::map_dfc(funs_mod, ~ unlist(purrr::map(., eval_tidy), recursive = FALSE, use.names = FALSE))
-
+expect_frame3 <- function(data, ...) {
+  mappers <- purrr::map(quos(...), purrr::as_mapper)
+  funs_mod <- purrr::map(data, ~ purrr::map(mappers, lang_modify, .))
+  return(funs_mod)
 }
 
-expect_frame3(airquality, ~ .x == -1, ~ .x == 2)
-vis_expect(iris,
+x <- expect_frame3(airquality, ~ .x == -1, ~ .x == 2)
+x[[1]](airquality)
+dvis_expect(iris,
            "ones" =  ~.x == -1,
            "twos" = ~.x == 2)
 
 purrr::map_dfc(iris, ~.x == -1)
+
+
