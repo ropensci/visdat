5ce1eaaedd00e48dacd402c59cb5affe90b9c001
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-12-15 19:27:25 +1100

    update vdiffr svg for vis-binary and vis-value

--- tests/testthat/test-vis-binary.R
+++ tests/testthat/test-vis-binary.R
@@ -4,8 +4,8 @@ vis_binary_plot <- vis_binary(dat_bin)
 
 test_that("vis_binary creates the right plot",{
   skip_on_cran()
-  skip_on_appveyor()
   skip_on_travis()
+  skip_on_appveyor()
   skip_on_gh_actions()
   vdiffr::expect_doppelganger("vis_binary vanilla",
                               vis_binary_plot)
